102437512dc1be097162fc9b5a2d2e53c9cd5973
Nate Rauh <nrauh@users.noreply.github.com>
2018-05-13 14:38:15 -0500

    Add blink API
    Can make the LED blink and change the rate of blink via PATCH request.
    
    Patch seemed more semantic than POST or PUT.

---
+++ API/API.ino
@@ -0,0 +1,78 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+
+ESP8266WebServer server(80);
+const int led = 2;
+
+bool ledBlinking = false;
+
+int ledState = LOW;     
+unsigned long previousMillis = 0;
+long blinkInterval = 1000;
+
+void blinkLed() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= blinkInterval) {
+    previousMillis = currentMillis;   
+
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    digitalWrite(led, ledState);
+  }
+}
+
+void connectToWiFi() {
+  WiFi.begin("SSID", "PASSWORD");
+
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Local IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void handleBlink() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(server.arg("plain"));
+ 
+  if (root.containsKey("blink")) {
+    ledBlinking = root["blink"];
+  }
+
+  if (root.containsKey("blinkRate")) {
+    blinkInterval = root["blinkRate"];
+  }
+
+  server.send(200, "text/plain", "I have blinked\n");
+}
+
+void setupServer() {
+  server.on("/blink", HTTP_PATCH, handleBlink);
+  server.begin();
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectToWiFi();
+  
+  pinMode(led, OUTPUT);
+
+  setupServer();
+}
+
+void loop() {
+  server.handleClient();
+
+  if (ledBlinking) {
+    blinkLed();
+  }
+}
